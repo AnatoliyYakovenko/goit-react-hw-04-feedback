8c7fdfce2197b92de1e860ef0e23993d452b66f6
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-03-09 11:15:51 +0200

    work with logic

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,22 @@
+import css from './FeedbackOptions.module.css';
+import PropTypes from "prop-types";
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  const optionsBtn = Object.keys(options);
+
+  return (
+    <ul className={css.btnList}>
+      {optionsBtn.map(option => (
+          <li key={ option}>
+          <button className={css[option]} type="button" onClick={() => onLeaveFeedback(option)}>
+            {option.toUpperCase()}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.objectOf(PropTypes.number),
+  onLeaveFeedback: PropTypes.func
+}
